4f5c6210ed0f41c70fafe1c11de195df24d5b3fa
Alcadis <andreasemenzato@hotmail.it>
2024-11-29 09:53:16 +0100

    new transit

--- src/daneel/detection/transit.py
+++ src/daneel/detection/transit.py
@@ -1,23 +1,23 @@
 import batman
 import numpy as np
 import matplotlib.pyplot as plt
-
+#HAT-P-18b hartman et al 2011
 params = batman.TransitParams()       #object to store transit parameters
 params.t0 = 0.                        #time of inferior conjunction
-params.per = 1.09141937               #orbital period
-params.rp = 0.195                     #planet radius (in units of stellar radii)
-params.a = 5.04                       #semi-major axis (in units of stellar radii)
-params.inc = 83.37                    #orbital inclination (in degrees)
-params.ecc = 0.0447                   #eccentricity
-params.w = 272.7                      #longitude of periastron (in degrees)
+params.per = 5.508                    #orbital period
+params.rp = 0.1365                    #planet radius (in units of stellar radii)
+params.a = 16.04                      #semi-major axis (in units of stellar radii)
+params.inc = 88.8                     #orbital inclination (in degrees)
+params.ecc = 0.084                    #eccentricity
+params.w = 120.0                      #longitude of periastron (in degrees)
 params.limb_dark = "quadratic"        #limb darkening model
-params.u = [0.574, 0.197]             #limb darkening coefficients [u1, u2, u3, u4]
+params.u = [0.506, 0.128]             #limb darkening coefficients [u1, u2, u3, u4]
 
 t = np.linspace(-0.10,0.10,100)              #times at which to calculate light curve
 m = batman.TransitModel(params, t)
 
 flux = m.light_curve(params)
-radii = np.array([0.0975, 0.1950, 0.3900])
+radii = np.array([0.06825, 0.1365, 0.273])
 fig = plt.figure(figsize=(10, 6))
 for r in radii:
         params.rp = r                           #updates planet radius
